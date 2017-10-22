32799edc3baa80bfcb3df62afed3c8ca41b7f1ba
magnussen7 <thibault.paquet@gmail.com>
2017-10-22 12:46:29 +0200

    Filtre terminé

--- SQLQuery.php
+++ SQLQuery.php
@@ -1,12 +1,12 @@
 <?php
   $queryGetAllCVEWithEditor = "SELECT * FROM cve, editeur WHERE cve.idEditeur = editeur.idEditeur ORDER BY dateCve DESC, statusCve";
   $queryGetAllCVEWithSomeEditor = "SELECT * FROM cve, editeur WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) ORDER BY dateCve DESC, statusCve";
-  $queryGetAllCVEWithSomeFaille = "SELECT * FROM cve, link_cve_faille WHERE cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) ORDER BY dateCve DESC, statusCve";
+  $queryGetAllCVEWithSomeFaille = "SELECT DISTINCT cve.idCve, cve.nomCve, cve.dateCve, cve.statusCve, cve.descriptionCve, cve.noteCve FROM cve, link_cve_faille WHERE cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) ORDER BY dateCve DESC, statusCve";
   $queryGetAllCVEWithStatus = "SELECT * FROM cve, editeur WHERE cve.idEditeur = editeur.idEditeur AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
-  $queryGetAllCVEWithSomeEditorAndSomeFaille =  "SELECT * FROM cve, editeur, link_cve_faille WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) AND cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) ORDER BY dateCve DESC, statusCve";
+  $queryGetAllCVEWithSomeEditorAndSomeFaille =  "SELECT DISTINCT cve.idCve, cve.nomCve, cve.dateCve, cve.statusCve, cve.descriptionCve, cve.noteCve, editeur.idEditeur, editeur.nomEditeur FROM cve, editeur, link_cve_faille WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) AND cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) ORDER BY dateCve DESC, statusCve";
   $queryGetAllCVEWithSomeEditorAndStatus = "SELECT * FROM cve, editeur WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
-  $queryGetAllCVEWithSomeFailleAndStatus = "SELECT * FROM cve, link_cve_faille WHERE cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
-  $queryGetAllCVEWithSomeEditorAndSomeFailleAndStatus = "SELECT * FROM cve, link_cve_faille, editeur WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) AND cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
+  $queryGetAllCVEWithSomeFailleAndStatus = "SELECT DISTINCT cve.idCve, cve.nomCve, cve.dateCve, cve.statusCve, cve.descriptionCve, cve.noteCve, editeur.idEditeur, editeur.nomEditeur FROM cve, link_cve_faille WHERE cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
+  $queryGetAllCVEWithSomeEditorAndSomeFailleAndStatus = "SELECT DISTINCT cve.idCve, cve.nomCve, cve.dateCve, cve.statusCve, cve.descriptionCve, cve.noteCve, editeur.idEditeur, editeur.nomEditeur FROM cve, link_cve_faille, editeur WHERE cve.idEditeur = editeur.idEditeur AND editeur.idEditeur IN (:arrayIdEditeur) AND cve.idCve = link_cve_faille.idCve AND link_cve_faille.idFaille IN (:arrayIdFaille) AND cve.statusCve = :statusCve ORDER BY dateCve DESC";
 
   $queryGetEditeur = "SELECT * FROM editeur";
 
--- ajax.php
+++ ajax.php
@@ -12,56 +12,27 @@
 		{
 			if (isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
 			{
-				if ($_POST['status'] == "all")
-				{
-					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "");
-				}
-				else if ($_POST['status'] == "open")
-				{
-					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), 1);
-				}
-				else if ($_POST['status'] == "close")
-				{
-					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), 0);
-				}
+					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), $_POST['status']);
 			}
 			else if (isset($_POST['editeur']) && isset($_POST['faille']) && !isset($_POST['status']))
 			{
-				return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "");
+					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "");
 			}
 			else if (isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
 			{
-				if ($_POST['status'] == "all")
-				{
-					return getListCVE(json_decode($_POST['editeur']), "", "");
-				}
-				else if ($_POST['status'] == "open")
-				{
-					return getListCVE(json_decode($_POST['editeur']), "", 1);
-				}
-				else if ($_POST['status'] == "close")
-				{
-					return getListCVE(json_decode($_POST['editeur']), "", 0);
-				}
+					return getListCVE(json_decode($_POST['editeur']), "", $_POST['status']);
 			}
 			else if (!isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
 			{
-				if ($_POST['status'] == "all")
-				{
-					return getListCVE("", json_decode($_POST['faille']), "");
-				}
-				else if ($_POST['status'] == "open")
-				{
-					return getListCVE("", json_decode($_POST['faille']), 1);
-				}
-				else if ($_POST['status'] == "close")
-				{
-					return getListCVE("", json_decode($_POST['faille']), 0);
-				}
+					return getListCVE("", json_decode($_POST['faille']), $_POST['status']);
+			}
+			else if (!isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
+			{
+					return getListCVE("", "", $_POST['status']);
 			}
 			else
 			{
-				return getListCVE("", "", "");
+					return getListCVE("", "", "");
 			}
 		}
 	}
