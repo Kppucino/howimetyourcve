fe65f560a8840cb9f97d9fc609266772b782ef3f
magnussen7 <thibault.paquet@gmail.com>
2017-10-23 21:35:15 +0200

    Pagination tableau

--- ajax.php
+++ ajax.php
@@ -4,35 +4,87 @@
 
 	if (isset($_POST["createTable"]))
 	{
+		if (isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
+		{
+				$nbPage = ceil(getNbCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), $_POST['status'])[0]["Nb"]/25);
+		}
+		else if (isset($_POST['editeur']) && isset($_POST['faille']) && !isset($_POST['status']))
+		{
+				$nbPage = ceil(getNbCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "")[0]["Nb"]/25);
+		}
+		else if (isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
+		{
+				$nbPage = ceil(getNbCVE(json_decode($_POST['editeur']), "", $_POST['status'])[0]["Nb"]/25);
+		}
+		else if (!isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
+		{
+				$nbPage = ceil(getNbCVE("", json_decode($_POST['faille']), $_POST['status'])[0]["Nb"]/25);
+		}
+		else if (!isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
+		{
+				$nbPage = ceil(getNbCVE("", "", $_POST['status'])[0]["Nb"]/25);
+		}
+		else
+		{
+				$nbPage = ceil(getNbCVE("", "", "")[0]["Nb"]/25);
+		}
+
+		if (isset($_POST["previous"]))
+		{
+			if ($_POST["previous"] - 1 >= 0)
+			{
+				$page = $_POST["previous"] - 1;
+			}
+			else
+			{
+				$page = $_POST["previous"];
+			}
+		}
+		else if (isset($_POST["next"]))
+		{
+			if ($_POST["next"] + 1 < $nbPage)
+			{
+				$page = $_POST["next"] + 1;
+			}
+			else
+			{
+				$page = $_POST["next"];
+			}
+		}
+		else
+		{
+			$page = 0;
+		}
+
 		if ($_POST["createTable"] == "empty")
 		{
-			return getListCVE("", "", "");
+			return getListCVE("", "", "", $page);
 		}
 		else
 		{
 			if (isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
 			{
-					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), $_POST['status']);
+					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), $_POST['status'], $page);
 			}
 			else if (isset($_POST['editeur']) && isset($_POST['faille']) && !isset($_POST['status']))
 			{
-					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "");
+					return getListCVE(json_decode($_POST['editeur']), json_decode($_POST['faille']), "", $page);
 			}
 			else if (isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
 			{
-					return getListCVE(json_decode($_POST['editeur']), "", $_POST['status']);
+					return getListCVE(json_decode($_POST['editeur']), "", $_POST['status'], $page);
 			}
 			else if (!isset($_POST['editeur']) && isset($_POST['faille']) && isset($_POST['status']))
 			{
-					return getListCVE("", json_decode($_POST['faille']), $_POST['status']);
+					return getListCVE("", json_decode($_POST['faille']), $_POST['status'], $page);
 			}
 			else if (!isset($_POST['editeur']) && !isset($_POST['faille']) && isset($_POST['status']))
 			{
-					return getListCVE("", "", $_POST['status']);
+					return getListCVE("", "", $_POST['status'], $page);
 			}
 			else
 			{
-					return getListCVE("", "", "");
+					return getListCVE("", "", "", $page);
 			}
 		}
 	}
